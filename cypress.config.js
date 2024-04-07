0324596dab10f0c6b89cf508a903ca0427f6b4b7
lediosax5 <guido_hit@hotmail.com>
2024-04-07 16:40:44 -0300

    update

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: "cypress/suits/*.{js,feature}",
+    specPattern: "cypress/e2e/*.{js,feature}",
     baseUrl: 'https://pushing-it.vercel.app/',
     viewportWidth: 1920,
     viewportHeight: 1080,
---
+++ cypress/e2e/register.js
@@ -0,0 +1,39 @@
+// -Imports
+
+// -Constants
+//const register = " ";
+const randomNumber = Math.floor(Math.random() * 10200);
+
+// -Tests
+describe("Positive register", function(){
+    it.only("random user AC", function(){
+        cy.visit(" ");
+        cy.get('[data-cy="user"]').should('have.attr', 'name', 'user').click();
+        cy.get('[data-cy="user"]').type("name" + randomNumber)
+        cy.get('[data-cy="pass"]').should('have.attr', 'name', 'pass').click();
+        cy.get('[data-cy="pass"]').type("pass" + randomNumber + "#")
+        cy.get("[value='Male']").check({force:true});
+        cy.get('[data-cy="day"]').select(20);
+        cy.get('[data-cy="month"]').select("May");
+        cy.get('[data-cy="year"]').select("1990");
+        cy.get('[data-cy="submitForm"]').should('have.attr', 'type', 'submit').click({force:true});
+    });
+})
+
+describe("Negative register", function(){
+    it("empty fields", function(){
+        cy.visit(" ");
+
+    });
+})
+describe("Positive login AC", function(){
+    it.only("normal login", function(){
+        cy.visit(" ");
+        cy.get('[data-cy="registertoggle"]').dblclick();
+        cy.get('[data-cy="user"]').should('have.attr', 'name', 'user').click();
+        cy.get('[data-cy="user"]').type("pushingit")
+        cy.get('[data-cy="pass"]').should('have.attr', 'name', 'pass').click();
+        cy.get('[data-cy="pass"]').type("123456!")
+        cy.get('[data-cy="submitForm"]').should('have.attr', 'type', 'submit').click({force:true});
+    });
+})
