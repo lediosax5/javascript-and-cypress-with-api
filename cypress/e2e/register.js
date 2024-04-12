3f2e6ece0161c491c1f257788ae5a41910af6c53
lediosax5 <guido_hit@hotmail.com>
2024-04-11 22:01:22 -0300

    update

--- cypress/e2e/register.js
+++ cypress/e2e/register.js
@@ -37,3 +37,18 @@ describe("Positive login AC", function(){
         cy.get('[data-cy="submitForm"]').should('have.attr', 'type', 'submit').click({force:true});
     });
 })
+
+//Modifica todos los selectores para obtener los mismos elementos web pero con rutas diferentes
+
+describe("Actividad complementaria 2", () =>{
+    it('Actividad complementeria 2', () =>{
+    cy.visit('');
+    cy.get('[data-cy="user"]').type('pushingit' + randomNumber);
+    cy.get('[data-cy="pass"]').type('123456!');
+    cy.get('[value="Female"]').check({force: true});
+    cy.get('[data-cy="day"]').select('3');
+    cy.get('[data-cy="month"]').select('August');
+    cy.get('[data-cy="year"]').select('1988');
+    cy.get('[data-cy="submitForm"]').click();
+    });
+});
