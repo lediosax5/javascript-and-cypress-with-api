19f691d15dc51ee9914451495d342aac48f5cd6f
lediosax5 <guido_hit@hotmail.com>
2024-04-12 22:58:55 -0300

    update

---
+++ cypress/e2e/desafios.js
@@ -0,0 +1,27 @@
+// -Imports
+[]
+
+// -Constants
+//const register = " ";
+const randomNumber = Math.floor(Math.random() * 10200);
+
+// -Tests
+describe("Desafios", function(){
+    it.only("desafio 1", function(){
+        cy.visit(" ");
+        cy.get('[data-cy="user"]').should('have.attr', 'name', 'user').click();
+        cy.get('[data-cy="user"]').type("unname" + randomNumber)
+        cy.get('[data-cy="pass"]').should('have.attr', 'name', 'pass').click();
+        cy.get('[data-cy="pass"]').type("unpass" + randomNumber + "!")
+        cy.get("[value='Male']").check({force:true});
+        cy.get('[data-cy="day"]').select(20);
+        cy.get('[data-cy="month"]').select("May");
+        cy.get('[data-cy="year"]').select("1990");
+        cy.get('button[data-cy="submitForm"]').should('have.attr', 'type', 'submit').click({force:true});
+        // on
+        cy.get("#todolistlink").click();
+        cy.get(".css-ha1fhc").find('[data-cy="task"]').type("tarea imaginaria");
+        cy.get(".css-ha1fhc").find('[data-cy="sendTask"]').click();
+        cy.get(".css-ha1fhc").contains("tarea imaginaria").siblings("button").click();
+    });
+})
--- cypress/e2e/register.js
+++ cypress/e2e/register.js
@@ -1,4 +1,5 @@
 // -Imports
+[]
 
 // -Constants
 //const register = " ";
@@ -6,7 +7,7 @@ const randomNumber = Math.floor(Math.random() * 10200);
 
 // -Tests
 describe("Positive register", function(){
-    it.only("random user AC", function(){
+    it("random user AC", function(){
         cy.visit(" ");
         cy.get('[data-cy="user"]').should('have.attr', 'name', 'user').click();
         cy.get('[data-cy="user"]').type("name" + randomNumber)
@@ -19,7 +20,6 @@ describe("Positive register", function(){
         cy.get('[data-cy="submitForm"]').should('have.attr', 'type', 'submit').click({force:true});
     });
 })
-
 describe("Negative register", function(){
     it("empty fields", function(){
         cy.visit(" ");
@@ -27,7 +27,7 @@ describe("Negative register", function(){
     });
 })
 describe("Positive login AC", function(){
-    it.only("normal login", function(){
+    it("normal login", function(){
         cy.visit(" ");
         cy.get('[data-cy="registertoggle"]').dblclick();
         cy.get('[data-cy="user"]').should('have.attr', 'name', 'user').click();
@@ -37,18 +37,16 @@ describe("Positive login AC", function(){
         cy.get('[data-cy="submitForm"]').should('have.attr', 'type', 'submit').click({force:true});
     });
 })
-
 //Modifica todos los selectores para obtener los mismos elementos web pero con rutas diferentes
-
-describe("Actividad complementaria 2", () =>{
-    it('Actividad complementeria 2', () =>{
-    cy.visit('');
-    cy.get('[data-cy="user"]').type('pushingit' + randomNumber);
-    cy.get('[data-cy="pass"]').type('123456!');
-    cy.get('[value="Female"]').check({force: true});
-    cy.get('[data-cy="day"]').select('3');
-    cy.get('[data-cy="month"]').select('August');
-    cy.get('[data-cy="year"]').select('1988');
-    cy.get('[data-cy="submitForm"]').click();
+describe("Actividad complementaria 2", function(){
+    it.only("random user AC con otros selectores", function(){
+        cy.visit(" ");
+        cy.get('input[id="user"]').type("elname" + randomNumber)
+        cy.get('input[id=pass]').type("elpass" + randomNumber + "#")
+        cy.get("input[value='Male']").check({force:true});
+        cy.get('select[id=day]').select(18);
+        cy.get("div").children('select[id=month]').select("May");
+        cy.get("#year").select("1980");
+        cy.get("form").find("button").click({force:true});
     });
 });
